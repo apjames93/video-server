a5a510bb69e2d0fd813f048f26ec74fe64c94499
Alexander James <apjames93@gmail.com>
2016-09-03 22:39:40 -0600

    setting up routs to get data and add data

--- api/video.js
+++ api/video.js
@@ -3,11 +3,16 @@ var router = express.Router();
 var knex = require('../db/knex');
 var queries = require('../queries/apiQueries');
 
-router.get('/:id', function(req, res, next){
-  queries.getUserData(req.params.id)
-  .then(function(data){
-    console.log(data);
-    res.json({user : data});
+  router.get('/:id', function(req, res, next){
+    queries.getUserData(req.params.id)
+    .then(function(data){
+      res.json({user : data});
+    });
   });
-});
+  router.post('/', function(req, res, next){
+    queries.addVideo(req.query, req.query.id[0])
+    .then(function(data){
+      res.json({itIS : "done"});
+    });
+  });
 module.exports = router;
--- queries/apiQueries.js
+++ queries/apiQueries.js
@@ -11,11 +11,25 @@ module.exports = {
     return knex('users').insert(body);
   },
   getUserData : function(id){
-      return knex('users').where({id: id});
+      return knex('users').innerJoin('users_videos', 'users_id', 'users.id').innerJoin('video','video_id', 'video.id');
   },
   getData : function(){
-    return knex('users')
-  }
+    return knex('users');
+  },
+
+
+
+  addVideo : function(video, userid){
+    // console.log('added to video table');
+  return knex('video').insert({
+    'youtube': video.youtube,
+    'reaction': video.reaction
+  }).returning('id')
+  .then(function(data){
+    console.log(data[0], "adding to join in add video ");
+  return knex('users_videos').insert({users_id: 1, video_id : data[0]}).returning('id');
+});
+}
 
 
 };
